88a4ffa7fd8ed6d49b3b5538e66ae89e6340f17b
imMaturana <git@maturanei.anonaddy.com>
2022-07-17 17:47:08 -0400

    stacktile: 2022-02-08 -> 0.1.1

--- pkgs/stacktile/default.nix
+++ pkgs/stacktile/default.nix
@@ -2,21 +2,20 @@
 , lib
 , fetchFromSourcehut
 , wayland
-, wayland-protocols
 }:
 
 stdenv.mkDerivation rec {
   pname = "stacktile";
-  version = "2022-02-08";
+  version = "0.1.1";
 
   src = fetchFromSourcehut {
     owner = "~leon_plickat";
     repo = pname;
-    rev = "10c25adfaec995967088f78ed9ddc0203022bbef";
-    sha256 = "05xszs8naxqrn7dp9m3232j6lr8flppq4fywqhfcr4w3qzihp37s";
+    rev = "v${version}";
+    sha256 = "sha256-1189DlhEf/w7mjg+RAXu6qGM6Xw6/KGS0oNUPnKoD8k=";
   };
 
-  buildInputs = [ wayland wayland-protocols ];
+  buildInputs = [ wayland ];
 
   installFlags = [ "PREFIX=$(out)" ];
 
